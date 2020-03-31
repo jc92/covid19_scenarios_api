e6182ec1786a2b665b764cf131b2bd8fab6fb208
Bruno RZN <bruno@velours.io>
2020-03-31 22:57:21 +0200

    added NODE_ENV to environment

--- src/utils/getEnvironment.ts
+++ src/utils/getEnvironment.ts
@@ -3,6 +3,7 @@ import { config } from 'dotenv';
 config({ path: `${process.cwd()}/.env` });
 
 type ConfigType = {
+  NODE_ENV: string;
   APP_NAME: string;
   APP_HOST: string;
   APP_PORT: number;
@@ -24,6 +25,7 @@ export function getEnvAsBoolean(key: string): boolean {
 export default function getEnvironment() {
   if (env === null) {
     env = {
+      NODE_ENV: getEnv('NODE_ENV', 'production', false),
       APP_NAME: getEnv('APP_NAME', '', true),
       APP_HOST: getEnv('APP_HOST', 'localhost', false),
       APP_PORT: Number(getEnv('APP_PORT', '5000', false)),
