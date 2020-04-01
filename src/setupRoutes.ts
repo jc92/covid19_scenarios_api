29e01aa4c8216a0aee42c1cfa9362cff8ca66f7a
Bruno RZN <bruno@velours.io>
2020-04-01 15:42:14 +0200

    added binary type

--- src/setupRoutes.ts
+++ src/setupRoutes.ts
@@ -2,13 +2,12 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 import health from './routes/health';
 import runSimulation from './routes/runSimulation';
-import { Context } from './types';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const router = require('koa-router')();
 
 export default function setupRoutes(app: Koa): void {
   router.get('/health', koaBody(), health);
-  router.get('/run', koaBody(), (ctx: Context) => runSimulation(ctx, true));
+  router.get('/run/:resultType', koaBody(), runSimulation);
   app.use(router.routes());
 }
