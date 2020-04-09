91b6f0d3120dab7376548ed9fc3130f834cc5a5a
Bruno RZN <bruno@velours.io>
2020-04-09 16:59:37 +0200

    setupRoutes now uses new runSimulation

--- src/setupRoutes.ts
+++ src/setupRoutes.ts
@@ -1,7 +1,7 @@
 import Koa from 'koa';
 import koaBody from 'koa-body';
 import health from './routes/health';
-import runSimulation from './routes/runSimulation';
+import runSimulation from './routes/runSimulation2';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const router = require('koa-router')();
