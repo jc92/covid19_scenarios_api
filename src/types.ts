a10aa72e7e7b15db1b10f9462cb6a84f5a4f033e
Bruno RZN <bruno@velours.io>
2020-04-09 17:01:59 +0200

    removed unused code !

--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,3 @@
 import { Context as KoaContext } from 'koa';
-import { ScenarioData } from './algorithms/types/Param.types';
 
 export type Context = KoaContext;
-
-export interface SeverityTableRow {
-  id: number;
-  ageGroup: string;
-  confirmed: number;
-  severe: number;
-  critical: number;
-  fatal: number;
-  totalFatal?: number;
-  isolated?: number;
-  errors?: {
-    confirmed?: string;
-    severe?: string;
-    critical?: string;
-    fatal?: string;
-    isolated?: string;
-  };
-}
-
-export interface State {
-  scenarios: string[];
-  current: string;
-  data: ScenarioData;
-}
